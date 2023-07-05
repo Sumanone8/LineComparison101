8ad2c56851f4ba383c29da96868f03acc1bbce18
Sumanone8 <sumanjhabhl@gmail.com>
2023-07-05 23:53:40 +0530

    Suman : Welcome Massage for Line Comparison Computation Problem

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LineComparison101
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Fellow learners, Welcome to Line Comparison Computation Program");
+
+            LineComparison lineComparison = new LineComparison();
+            lineComparison.CalculateLineLength();
+        }
+    }
+}
